680e5ff73c1f20c3ed936dadd4feb09be9ba5eca
Moch Fauzi <mochfauzi57@gmail.com>
2023-06-04 10:16:42 +0700

    Clean the Project and prepare it for CRUD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');
     }
 
     /**
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Hallo Selamat Datang {{ $user->name }}</h1>
+    <h1>Hallo Selamat Datang {{ $user != null ? $user->name: "" }}</h1>
 @endsection
